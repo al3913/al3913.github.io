78d631bd5c33bbcabf61486cb14a6a510038b624
al3913 <113476449+al3913@users.noreply.github.com>
2025-02-10 22:21:24 -0500

    Clickable card

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById("surpriseBtn").addEventListener("click", function() {
+document.getElementById("card").addEventListener("click", function() {
     const message = document.getElementById("surpriseMessage");
     message.classList.toggle("hidden");
     message.style.animation = "fadeIn 1s ease-in-out";
